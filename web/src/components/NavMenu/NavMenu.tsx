982550565a870235ac598f308fd2f42e8d303851
Kira <hcq_pm@163.com>
2023-06-26 00:10:26 +0800

    layout-nav

--- web/src/components/NavMenu/NavMenu.tsx
+++ web/src/components/NavMenu/NavMenu.tsx
@@ -1,37 +1,68 @@
 import React, { Component } from 'react'
-import { Header, Menu, Segment } from 'semantic-ui-react'
-
+import { Header, Menu, Segment,Container,Input  } from 'semantic-ui-react'
+import style from "../../assets/css/common.module.scss"
 type NavItem = {
-    name:string
+  key:string,
+  label:string,
+}
+type Props = {
+  
 }
 
 export default class MenuExampleInvertedSegment extends Component {
-  state = { activeItem: 'home' }
+  state
+  constructor(props:Props) {
+    super(props)
+    const navList: NavItem[] = [
+      {
+        key:"blog",
+        label:"博客"
+      },
+      {
+        key:"archives",
+        label:"归档"
+      },
+      {
+        key:"experiment",
+        label:"实验中心"
+      }
+    ]
+    this.state = {
+      activeIndex: 0 ,
+      navList
+    }
+  }
 
-  handleItemClick = (_e: any, { name }:NavItem) => this.setState({ activeItem: name })
+  handleItemClick = (navItem:NavItem,index:number) => {
+    this.setState({ activeIndex: index })
+  }
 
   render() {
-    const { activeItem } = this.state
-
+    const { activeIndex ,navList} = this.state
     return (
-      <Segment inverted textAlign="center">
-        <Menu inverted secondary  >
-          <Menu.Item
-            name='home'
-            active={activeItem === 'home'}
-            onClick={this.handleItemClick}
-          />
-          <Menu.Item
-            name='messages'
-            active={activeItem === 'messages'}
-            onClick={this.handleItemClick}
-          />
-          <Menu.Item
-            name='friends'
-            active={activeItem === 'friends'}
-            onClick={this.handleItemClick}
-          />
-        </Menu>
+      <Segment className= {`${style.padding_tb_small}`} inverted textAlign="center" basic as='nav'  attached >
+        <Container textAlign='center'>
+          <Menu inverted secondary  >
+            <Menu.Item>
+              <Header as='h1' inverted color='teal'>
+                Brad
+              </Header>
+            </Menu.Item>
+            {
+              navList.map((item,index) => (
+                <Menu.Item 
+                  key={item.key}
+                  name={item.label}
+                  active={index== activeIndex}
+                  onClick={()=>this.handleItemClick(item,index)}
+                ></Menu.Item>
+              ))
+            }
+            <Menu.Item position='right'>
+              <Input placeholder="Search..." icon='search' ></Input>
+            </Menu.Item>
+          </Menu>
+        </Container>
       </Segment>
     )
   }
