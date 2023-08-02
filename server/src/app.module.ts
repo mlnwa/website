49433a392656978eae147da9ea017267880783bc
Kira <hcq_pm@163.com>
2023-08-03 00:25:29 +0800

    http-exception filter

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,30 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 @Module({
-  imports: [],
+  imports: [
+    
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    /**
+     * http-exception local filter
+     */
+    {
+      provide:APP_FILTER,
+      useClass:HttpExceptionFilter
+    }
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consume:MiddlewareConsumer){
+    consume
+      .apply(LoggerMiddleware)
+      .forRoutes('*')
+  }
+}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
+const listenPort = 3000
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  /**
+   * http-exception global filter
+   */
+  // app.useGlobalFilters(new HttpExceptionFilter())
+
+  await app.listen(listenPort);
 }
 bootstrap();
