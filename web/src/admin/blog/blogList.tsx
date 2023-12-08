021915bcff9c6da0a1100e05608f74abc676bc4e
Alan <hcq_pm@163.com>
2023-12-08 23:34:38 +0800

    sup page : blogList #1

--- web/src/admin/blog/blogList.tsx
+++ web/src/admin/blog/blogList.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { Container, Segment } from 'semantic-ui-react';
+import { Button, Container, Flag, Input, Label, Segment } from 'semantic-ui-react';
 import ITable, { ColumnType } from '../../components/ITable';
+import { DatePicker, Drawer } from 'antd';
+import { IMessage } from '../../components/IMessage';
 
 const BlogList = function () {
+  const [open, setOpen] = React.useState(false);
   const columns: ColumnType[] = [
     {
       title: '标题',
@@ -29,7 +32,28 @@ const BlogList = function () {
       name: '111',
     },
   ];
-  return <ITable list={list} columns={columns}></ITable>;
+  const onClose = () => {
+    setOpen(false);
+  };
+  return (
+    <div>
+      <Label>
+        时间：
+        <DatePicker size="small"></DatePicker>
+      </Label>
+
+      <Button icon="search" content="搜索" size="small"></Button>
+      <Button icon="filter" content="筛选条件" size="small" onClick={() => setOpen(true)}></Button>
+      <ITable list={list} columns={columns}></ITable>
+      <Drawer open={open} onClose={onClose} placement="right" width={600} title="筛选条件">
+        <Input label="作者" size="small"></Input>
+        <Input label="状态" size="small"></Input>
+        <Input label="专栏" size="small"></Input>
+        <Input label="类别" size="small"></Input>
+        <Input label="标签" size="small"></Input>
+      </Drawer>
+    </div>
+  );
 };
 
 export default BlogList;
