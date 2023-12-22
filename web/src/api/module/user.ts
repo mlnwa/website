6e83ca8af3db7652588e5719bf6decdaf9cace4d
Alan <hcq_pm@163.com>
2023-12-23 00:15:18 +0800

    -

--- web/src/api/module/user.ts
+++ web/src/api/module/user.ts
@@ -4,13 +4,13 @@ export interface LoginParam {
   username: string;
   password: string;
 }
-type LoginInfo = {
+type LoginResult = {
   accessToken: string;
   freshToken: string;
 };
 const URL = '/auth';
 export const Login = (data: LoginParam) =>
-  http.post<LoginInfo>({
+  http.post<LoginResult>({
     url: `${URL}/login`,
     data,
   });
