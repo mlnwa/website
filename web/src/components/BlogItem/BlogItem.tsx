f60f362d840a01fdc81d29da9432c93ab1878db2
Alan <hcq_pm@163.com>
2024-01-18 00:48:14 +0800

    feat : blog page date

--- web/src/components/BlogItem/BlogItem.tsx
+++ web/src/components/BlogItem/BlogItem.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button, Grid, GridColumn, Header, Icon, Image, Item, List, Message, Segment } from 'semantic-ui-react';
 import style from '../../assets/css/common.module.scss';
 import { Blog } from '../../api/module/blog';
+import { DateUtil } from '../../utils';
 
 type Props = {
   value: Blog;
@@ -17,25 +18,26 @@ const BlogItem = function (props: Props) {
           <Header as="h3" content={blog.title}></Header>
           <p className={style.m_text}>{blog.abstract}</p>
           <Grid>
-            <Grid.Column width={11}>
+            <Grid.Column width={13}>
               <List size="mini" horizontal link>
                 <Item className="middle aligned">
                   <Image avatar src={blog.userAvatarUrl}></Image>
                   <Item.Content>
-                    <Header as="a" content="brad"></Header>
+                    <Header as="a" content={blog.userName}></Header>
                   </Item.Content>
                 </Item>
                 <Item className="middle aligned">
                   <Icon name="calendar"></Icon>
                   {blog.createAt}
                 </Item>
+                <Item className="middle aligned">更新于{DateUtil.day(blog.updateAt).convertToTimeAgo()}</Item>
                 <Item className="middle aligned">
                   <Icon name="eye"></Icon>
                   {blog.viewCount}
                 </Item>
               </List>
             </Grid.Column>
-            <Grid.Column width={5}></Grid.Column>
+            <Grid.Column width={3}></Grid.Column>
           </Grid>
         </Grid.Column>
         <Grid.Column width={5}>
