e907c1e6b94162eb44a48584b0fd7095b219145e
Alan <hcq_pm@163.com>
2024-01-21 20:36:54 +0800

    feat : cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,7 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  app.enableCors();
   await app.listen(configService.get('app').port);
 }
 bootstrap();
