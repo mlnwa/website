060a8f0dc9d11862bfd5f930af80f19d70ef7838
Alan <hcq_pm@163.com>
2024-01-24 15:51:47 +0800

    feat

--- web/src/utils/TypeUtil.ts
+++ web/src/utils/TypeUtil.ts
@@ -1,25 +1,30 @@
-function isType(type:string){
-    return function(data:any):boolean{
-        const toStringType =  Object.prototype.toString.call(data) as string
-        return toStringType.substring(8,toStringType.length-1) == type
-    }
+function isType(type: string) {
+  return function (data: any): boolean {
+    const toStringType = Object.prototype.toString.call(data) as string;
+    return toStringType.substring(8, toStringType.length - 1) == type;
+  };
 }
-function isJSONType(type:string){
-    return function(data:any):boolean{
-        try {
-            data = JSON.parse(data)
-            return isType(type)(data)
-        } catch (error) {
-            return false
-        }
+function isJSONType(type: string) {
+  return function (data: any): boolean {
+    try {
+      data = JSON.parse(data);
+      return isType(type)(data);
+    } catch (error) {
+      return false;
     }
+  };
 }
 export const TypeUtil = class {
-    static isArray = isType("Array")
-    static isString = isType('String')
-    static isObject = isType("Object")
+  static isArray = isType('Array');
+  static isString = isType('String');
+  static isObject = isType('Object');
+  static isUndefined = isType('Undefined');
+  static isNull = isType('Null');
+  static isNumber = isType('Number');
+  static isFunction = isType('Function');
+  static isDate = isType('Date');
 
-    static isJSONArray = isJSONType("Array")
-    static isJSONString = isJSONType("String")
-    static isJSONObject = isJSONType("Object")
-}
+  static isJSONArray = isJSONType('Array');
+  static isJSONString = isJSONType('String');
+  static isJSONObject = isJSONType('Object');
+};
