4a6d617f0efb4d000851be9ccd8bb74ac77e602b
Alan <hcq_pm@163.com>
2024-02-26 20:54:14 +0800

    feat : user api

--- web/src/api/module/user.ts
+++ web/src/api/module/user.ts
@@ -1,16 +1,45 @@
 import http from '../requrest';
+import { PageParams, Pagination } from '../type';
 
 export interface LoginParam {
   username: string;
   password: string;
+}
+export interface User {
+  id: number;
+  uid: string;
+  name: string;
+  email: string;
+  status: number;
+  createAt: string;
+  updateAt: string;
 }
 type LoginResult = {
   accessToken: string;
   freshToken: string;
 };
 const URL = '/auth';
+interface QueryUserListParam extends PageParams {}
+interface UpdateUserData extends Partial<User> {}
 export const Login = (data: LoginParam) =>
   http.post<LoginResult>({
     url: `${URL}/login`,
+    data,
+  });
+
+export const QueryUserList = (params: QueryUserListParam) =>
+  http.get<Pagination<User>>({
+    url: `${URL}/list`,
+    params,
+  });
+
+export const DeleteUser = (id: number) =>
+  http.delete<boolean>({
+    url: `${URL}/${id}`,
+  });
+
+export const UpdateUser = (id: number, data: UpdateUserData) =>
+  http.put<boolean>({
+    url: `${URL}/${id}`,
     data,
   });
